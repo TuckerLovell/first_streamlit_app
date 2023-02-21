311a5ffcc1a4ae0939f13f6d12a75ca809e94874
TuckerLovell <105234611+TuckerLovell@users.noreply.github.com>
2023-02-21 15:24:54 -0700

    Update streamlit_app.py
    added a widget for choosing fruit in smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,9 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍓🥝Build Your OWN Fruit Smoothie 🍌🍇')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+#Widget for picking fruit in smoothie
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.dataframe(my_fruit_list)

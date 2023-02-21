9f51965b541921827951bc96818d8439514a1e39
TuckerLovell <105234611+TuckerLovell@users.noreply.github.com>
2023-02-21 15:14:38 -0700

    Update streamlit_app.py
    Followed the instructions and changed the theme of the app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 
-streamlit.title("Snowflake/Streamlit Practice")
-streamlit.header("This is a sample header")
-streamlit.text("This is some sample text. Pretty nice.")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
